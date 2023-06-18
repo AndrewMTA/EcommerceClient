49d5cb017bd10a1a9700d9f3e87ae04574f577f0
AndrewMTA <2353andrew@gmail.com>
2023-06-18 12:24:22 -0500

    fa

--- src/services/appApi.js
+++ src/services/appApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const appApi = createApi({
     reducerPath: "appApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://pizzaserver.onrender.com" }),
     endpoints: (builder) => ({
         signup: builder.mutation({
             query: (user) => ({
